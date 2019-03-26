9cb0dd03eeaad4067a8489f788e686fb4ab34979
Igor Ramadas <igor@devv.com>
2019-03-26 21:40:58 +0100

    Updated docs and specs.

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # Anyhow
 
-Drop-in logging wrapper for Winston and console.
-
 [![Build Status](https://img.shields.io/travis/igoramadas/anyhow.svg?style=flat-square)](https://travis-ci.org/igoramadas/anyhow)
 [![Coverage Status](https://img.shields.io/coveralls/github/igoramadas/anyhow.svg?style=flat-square)](https://coveralls.io/github/igoramadas/anyhow?branch=master)
+
+Drop-in logging wrapper for Winston and console.
 
 ## Which modules use this?
 
---
+++ lib/index.d.ts
@@ -0,0 +1,81 @@
+/**
+ * Anyhow
+ */
+declare class Anyhow {
+    private static _instance;
+    static readonly Instance: Anyhow;
+    /**
+     * Default is true. Messages will be compacted (spaces and breaks removed).
+     * Set to false to log original values including spaces.
+     */
+    compact: boolean;
+    /**
+     * A function that should be called to preprocess the arguments.
+     * This is useful if you wish to remove or obfuscate data before
+     * generating the logging message.
+     */
+    preprocessor: Function;
+    /**
+     * Set the separator between arguments when generating logging messages.
+     * Default separator is a vertical line " | ".
+     */
+    separator: string;
+    /**
+     * Default console logging styles to be used in case the chalk module is installed.
+     */
+    styles: any;
+    /**
+     * Private logging function. This will be set by the constructor
+     * depending on which logger library is used.
+     */
+    private _log;
+    /**
+     * Default logging method.
+     * @param level String representing the level: error, warn, info, verbose, debug, silly
+     * @param args Array of arguments to be logged.
+     * @returns The generated message that was just logged.
+     */
+    log(level: string, args: any): string;
+    /**
+     * Shortcut to log("debug", args).
+     */
+    debug(): string;
+    /**
+     * Shortcut to log("info", args).
+     */
+    info(): string;
+    /**
+     * Shortcut to log("warn", args).
+     */
+    warn(): string;
+    /**
+     * Shortcut to log("error", args).
+     */
+    error(): string;
+    /**
+     * Log directly to the console. This is the default logger handler
+     * in case no other compatible libraries are found.
+     * @param level String representing the level: error, warn, info, debug
+     * @param args Array of arguments to be logged.
+     * @returns The generated message that was just logged.
+     */
+    console(level: string, args: any): string;
+    /**
+     * Setup will try to load compatible loggers, and fall back
+     * to the console if nothing was found. You can also force
+     * a specific library to be loaded passing lib = "console|winston|
+     * @param lib Optional, force a specific library to be used. If not passed, will try winston first then console.
+     */
+    setup(lib?: string): void;
+    /**
+     * Default Anyhow constructor. Calls setup() by default.
+     */
+    constructor();
+    /**
+     * Gets a nice, readable message out of the passed array of arguments.
+     * @param originalArgs Any single or collection of objects that will be transformed to a message string.
+     */
+    getMessage(originalArgs: any): string;
+}
+declare const _default: Anyhow;
+export = _default;
